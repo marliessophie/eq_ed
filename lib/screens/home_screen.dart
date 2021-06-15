03f69e9d7b6a6bd653bd4dd9bd01717a82197269
MarliesMayerhofer <msm20@ic.ac.uk>
2021-06-15 17:58:10 +0100

    EmailLoginScreen, HomeScreen, LoginScreen, RegisterScreen, WelcomeScreen classes created to extend stful widgets. Route navigation added to main.dart. Constants file created.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  static var id = 'home_screen';
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

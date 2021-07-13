ca4d5b4bd472c4bd267854a8f84ce822488b360b
MarliesMayerhofer <msm20@ic.ac.uk>
2021-07-13 22:05:33 +0100

    Added main ToDos in main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:eq_ed/screens/home_flow/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+// TODO: work on interaction with back-end API endpoints >> build model (do research)
+/* TODO: change score display for users, as second screen to come up
+    when the user presses on their level, to see a description of the current level, and score split */
+// TODO: include graphical representation of actual snowballs / badges that users get while enhancing
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();

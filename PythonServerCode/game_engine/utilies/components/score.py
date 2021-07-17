6115c2e723e71ffdebe1723cce1c2cc04ece7a17
MarliesMayerhofer <msm20@ic.ac.uk>
2021-07-17 12:53:34 +0100

    Code refactored to seperate components into individual classes.

---
+++ PythonServerCode/game_engine/utilies/components/score.py
@@ -0,0 +1,15 @@
+import json
+
+# TODO: separate out classes into separate files
+
+
+class Score:
+    def __init__(self, communication, empathy):
+        self.empathy = empathy
+        self.communication = communication
+
+    def convert_to_json(self):
+        data = {'empathy': self.empathy, 'communication': self.communication}
+        json_object = json.dumps(data)
+        print(json_object)
+        return json_object

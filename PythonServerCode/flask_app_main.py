84eb0b11ebf23d65326aeeafe384362c43f84c4f
MarliesMayerhofer <msm20@ic.ac.uk>
2021-07-27 19:35:32 +0100

    Added third api endpoint for level end feedback, excluding scoring.

--- PythonServerCode/flask_app_main.py
+++ PythonServerCode/flask_app_main.py
@@ -73,6 +73,7 @@ def main_initLevelForUser_post():
         return json.dumps({'success': True, 'level_narrative': text, 'next_question_id': next_question_id}), \
                200, {'ContentType': 'application/json'}
 
+
 @app.route('/getQuestionResponse', methods=['POST'])  # check deployment on AWS with boyd
 def main_getQuestionResponse_post():
     if request.method == 'POST':
@@ -101,6 +102,35 @@ def main_getQuestionResponse_post():
 
         return json.dumps({'success': True, 'question_text': text, 'number_of_answers': number_of_answers,
                            'answers': answers}), \
+               200, {'ContentType': 'application/json'}
+
+
+@app.route('/getLevelEnd', methods=['POST'])  # check deployment on AWS with boyd
+def main_getLevelEnd_post():
+    if request.method == 'POST':
+        with open('json_schemas/schema_getLevelEnd_post.json') as sch_init:
+            sch_init = json.load(sch_init)
+            print(sch_init)
+
+        json_data = request.get_json()
+        print(json_data)
+        if not validate_json(json_data, sch_init):
+            raise InvalidUsage('Invalid json format. uid and level_id fields required',
+                               status_code=400)
+
+        print(f'json received is: {json_data}')
+        uid = json_data['uid']
+        question_id = json_data['question_id']
+
+        # engine = GameEngine()
+        question = get_level(question_id) # todo - need to rewrite this
+        # todo - include bool if user passed or not
+        text = question['question_text']
+        next_question_id = question['next_question_id']
+
+        # todo - score user by uid something like engine.score(uid, level_id)
+
+        return json.dumps({'success': True, 'question_text': text, 'next_question_id': next_question_id}), \
                200, {'ContentType': 'application/json'}
 
 
